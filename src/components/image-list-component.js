5031e1ba113e02117158d2013a8ef758dc1d3fa7
s183214 <45406081+s183214@users.noreply.github.com>
2021-01-21 22:10:55 +0100

    using ref's for dom access

--- src/components/image-list-component.js
+++ src/components/image-list-component.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "./image-list-component.css";
+import ImageCard from "./image-card-component";
 
 const ImageList = (props) => {
   const images = props.images.map((image) => {
     return (
-      <img key={image.id} src={image.urls.regular} alt={image.description} />
+      // <img key={image.id} src={image.urls.regular} alt={image.description} />
+      <ImageCard key={image.id} image={image}></ImageCard>
     );
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
