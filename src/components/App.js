3a9a52036b366370bd594477d238db1a638c1bcc
s183214 <45406081+s183214@users.noreply.github.com>
2021-01-22 16:03:32 +0100

    Hooks in React - search bar and wiki apis

--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,26 @@
 import React from "react";
-import SearchBar from "./search-bar-component";
-import youtube from "../api/youtube";
-import VideoList from "./video-list-component";
-import VideoDetails from "./video-detail-component";
+import Accordion from "./accordion-component";
+import Search from "./search-bar-component";
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front-end js framework.",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favourite js library among developers.",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components.",
+  },
+];
 
-  componentDidMount() {
-    //whatever we place here, it will be displayed to the user when first loads
-    this.onTermSubmit("cats"); //we are going to search for cats and show the results on the screen
-  }
-
-  onTermSubmit = async (term) => {
-    //get the newly fetched videos and return them as list
-    // console.log(term); //we get what the search term is
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    });
-
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
-  };
-
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
-  };
-
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              {" "}
-              <VideoDetails video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
-          </div>
-        </div>
-
-        {/* the results from the search will be stored in 'videos' and that's why we pass the elements to the component */}
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
--- src/components/search-bar-component.js
+++ src/components/search-bar-component.js
@@ -1,35 +1,81 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
-class SearchBar extends React.Component {
-  state = { term: "" };
+const Search = () => {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
 
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value });
-  };
+  //we need to tell it when we want it to be executed as a second element
+  //1. empty array [] (run at the initial render),
+  //2.  array with value/s [term], (at the initial render AND after every rerender IF data has changed since last render)
+  //3. or no array at all (at the initial render AND after every rerender)
+  //we can NOT mark the arrow function as async useEffect (async())<--NO
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+    //detect if this would be the first search - if yes - no timer - search immediatelly
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeOutId = setTimeout(() => {
+        //wait for the input 500ms
+        if (term) {
+          // check if term is an empty string
 
-  onFormSubmit = (event) => {
-    event.preventDefault();
-    //ToDo: Make sure we call call back from parent component
-    this.props.onFormSubmit(this.state.term);
-  };
+          search();
+        }
+      }, 500);
+      return () => {
+        //
+        clearTimeout(timeOutId);
+      };
+    }
+  }, [term]);
 
-  render() {
+  const renderedResults = results.map((result) => {
     return (
-      <div className="ui segment search bar">
-        <form className="ui form" onSubmit={this.onFormSubmit}>
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-            {/* we place value, to tell the component  */}
-          </div>
-        </form>
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
       </div>
     );
-  }
-}
+  });
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+          {/* everytime the value is changed, we are going to update the state that keep track of that state */}
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
 
-export default SearchBar;
+export default Search;
